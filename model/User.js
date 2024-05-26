82797e46b55a949a2e31cbfd818d4b288d4f4ab7
KIMMIJU <hviezda1212@gmail.com>
2024-05-27 01:57:48 +0900

    로그인 만들기

--- model/User.js
+++ model/User.js
@@ -1,17 +1,37 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const Schema = mongoose.Schema;
+require('dotenv').config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
-const userSchema = Schema({
-  name: {
-    type: String,
-    required: true,
+const userSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  password:{
-   type:String,
-   required:true,
-  }
-},{timestamps:true});
+  { timestamps: true }
+);
+
+userSchema.methods.toJSON = function () {
+  const obj = this._doc;
+  delete obj.password;
+  return obj;
+};
+
+userSchema.methods.generateToken = function () {
+  const token = jwt.sign({ _id: this.id }, JWT_SECRET_KEY, { expiresIn: '1d' });
+  return token;
+};
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
